754c207d5927e536c10f336dd35526dccf825b6d
Vlad Preda <icero.freeze@gmail.com>
2019-02-16 22:48:16 +0200

    added js and some more dependencies

--- scripts/sub-count.js
+++ scripts/sub-count.js
@@ -1,6 +1,10 @@
 const remote = require('electron').remote;
 var maximized = false;
 var socialblade = require('socialblade-data');
+var request = require("request");
+
+let pewds = 0;
+let tseries = 0;
 
 function loadListeners(){
     var window = remote.getCurrentWindow();
@@ -24,15 +28,55 @@ function loadListeners(){
         }
     });
 }
-function loadChannelData(){
-    socialblade.loadChannelData("UC03MC72mOqM02EwWGL-NGpA",function(err,data){
-        console.log(data);
-    });
-    
+setInterval(function(){
+    loadChannelData("PewDiePie",0);
+    loadChannelData("tseries",1);
+    console.log("loaded");
+},3000);
+setInterval(function(){
+    subDifference();
+},3050);
+function subDifference(){
+    document.getElementsByClassName("sub-diff-container")[0].getElementsByClassName("sub-diff-text")[0].innerHTML = "Pewds is ahead by: "+numberWithCommas(pewds-tseries);
 }
+function numberWithCommas(x) {
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+}
+function loadChannelData(username,number){
+    var url = "https://www.googleapis.com/youtube/v3/channels?part=statistics"+"&forUsername="+username+"&key=AIzaSyBwZlzp5SRyVQfL0PQt1NmIwdmsvKG1Z3M";
+
+    request({
+        url: url,
+        json: true
+    }, function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            if(body.items[0].statistics.subscriberCount!=null){
+                document.getElementsByClassName("sub-container")[number].getElementsByClassName("channel-subs")[0].innerHTML = numberWithCommas(body.items[0].statistics.subscriberCount);
+                switch (number) {
+                    case 0:
+                        pewds = body.items[0].statistics.subscriberCount;
+                    break;
+                    case 1:
+                        tseries = body.items[0].statistics.subscriberCount;
+                    break;
+                }
+            }
+            else{
+                console.log(body);
+            }
+        }
+    });
+}
+setTimeout(function(){
+    subDifference();
+},500)
 function start(){
     loadListeners();
-    loadChannelData();
+    loadChannelData("PewDiePie",0); loadChannelData("tseries",1);
+    subDifference();
 }
 
 document.addEventListener("DOMContentLoaded",function(){
